9b5e420e6d4026525c6e1bb1157ae4742ca6bcc5
WENZY <79008258+wenzyfyy@users.noreply.github.com>
2024-09-20 19:07:56 +0300

    Update app.js

--- src/base/app.js
+++ src/base/app.js
@@ -1,21 +1,20 @@
-// Bu dosya botun slash komutlarını eventlerini ve message komutlarını yüklemek içindir.
-
-const commands = [];
 const fs = require("node:fs");
 const path = require("node:path");
-const { REST } = require('@discordjs/rest');
-const { Collection } = require("discord.js");
-const { Routes } = require('discord-api-types/v9');
-const eventsPath = path.join(__dirname, "../events");
+const { REST, Routes, Collection, SlashCommandBuilder } = require('discord.js');
 const { botid } = require("./settings.json");
+
+const commands = [];
+const eventsPath = path.join(__dirname, "../events");
 const commandsPath = path.join(__dirname, "../commands");
-const { SlashCommandBuilder } = require('@discordjs/builders');
+
 const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"));
 const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
 
 module.exports = (client) => {
   client.commands = new Collection();
   client.slashcommands = new Collection();
+
+  // Eventleri yükleme
   for (const file of eventFiles) {
     const filePath = path.join(eventsPath, file);
     const event = require(filePath);
@@ -24,40 +23,82 @@ module.exports = (client) => {
     } else {
       client.on(event.name, (...args) => event.execute(...args, client));
     }
-  };
+  }
 
+  // Komutları yükleme
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
+
+    // Slash komutlarını işleme
     if (command.slash) {
-      client.slashcommands.set(command.name[0], command)
+      client.slashcommands.set(command.name[0], command);
       const slashCommand = new SlashCommandBuilder()
         .setName(command.name[0])
-        .setDescription(command.description)
-      if (command.option) {
-        for (i = 0; i < command.option.length; i++) {
-          if (command.option[i].type === 'string') slashCommand.addStringOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
-          if (command.option[i].type === 'integer') slashCommand.addIntegerOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
-          if (command.option[i].type === 'number') slashCommand.addNumberOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
-          if (command.option[i].type === 'boolean') slashCommand.addBooleanOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
-          if (command.option[i].type === 'user') slashCommand.addUserOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
-          if (command.option[i].type === 'channel') slashCommand.addChannelOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
-          if (command.option[i].type === 'role') slashCommand.addRoleOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
-          if (command.option[i].type === 'mentionable') slashCommand.addSMentionableOption(option => option.setName(command.option[i].name).setDescription(command.option[i].description).setRequired(command.option[i].require))
+        .setDescription(command.description);
+
+      if (command.options) {
+        for (let i = 0; i < command.options.length; i++) {
+          const option = command.options[i];
+          switch (option.type.toLowerCase()) {
+            case 'string':
+              slashCommand.addStringOption(opt => 
+                opt.setName(option.name).setDescription(option.description).setRequired(option.required));
+              break;
+            case 'integer':
+              slashCommand.addIntegerOption(opt => 
+                opt.setName(option.name).setDescription(option.description).setRequired(option.required));
+              break;
+            case 'number':
+              slashCommand.addNumberOption(opt => 
+                opt.setName(option.name).setDescription(option.description).setRequired(option.required));
+              break;
+            case 'boolean':
+              slashCommand.addBooleanOption(opt => 
+                opt.setName(option.name).setDescription(option.description).setRequired(option.required));
+              break;
+            case 'user':
+              slashCommand.addUserOption(opt => 
+                opt.setName(option.name).setDescription(option.description).setRequired(option.required));
+              break;
+            case 'channel':
+              slashCommand.addChannelOption(opt => 
+                opt.setName(option.name).setDescription(option.description).setRequired(option.required));
+              break;
+            case 'role':
+              slashCommand.addRoleOption(opt => 
+                opt.setName(option.name).setDescription(option.description).setRequired(option.required));
+              break;
+            case 'mentionable':
+              slashCommand.addMentionableOption(opt => 
+                opt.setName(option.name).setDescription(option.description).setRequired(option.required));
+              break;
+            default:
+              console.warn(`Geçersiz seçenek türü: ${option.type}`);
+          }
         }
       }
-      commands.push(slashCommand)
+      commands.push(slashCommand.toJSON());
     }
+
+    // Normal (prefixli) komutları işleme
     if (!command.slash) {
-      for (i = 0; i < command.name.length; i++) {
+      for (let i = 0; i < command.name.length; i++) {
         client.commands.set(command.name[i], command);
       }
     }
   }
 };
 
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+// REST API'yi kullanarak komutları Discord'a yükleme
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-setTimeout(async () => {
-  rest.put(Routes.applicationCommands(botid), { body: commands }).catch(console.error);
-}, 500);
+client.once('ready', async () => {
+  try {
+    console.log('Komutlar Discord API\'ye yükleniyor...');
+    await rest.put(Routes.applicationCommands(botid), { body: commands });
+    console.log('Tüm komutlar başarıyla yüklendi.');
+  } catch (error) {
+    console.error('Komutlar yüklenirken bir hata oluştu:', error);
+  }
+});
